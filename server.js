98e0ea040ec0aa3bab669bf5631de4a57851e687
David Cabrera <dcabrera1982@gmail.com>
2019-09-10 12:03:27 -0400

    added bodyParser info

--- server.js
+++ server.js
@@ -5,4 +5,9 @@ var path = require("path");
 var app = express();
 var port = process.env.PORT || 3000;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json"}));
+
 app.listen(port, () => console.log("Listening on port %s", port));
