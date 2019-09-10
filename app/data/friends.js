3b09fa92faf0d629f8b33d5dc1beb92bca83a086
David Cabrera <dcabrera1982@gmail.com>
2019-09-10 12:45:57 -0400

    friends and scores

---
+++ app/data/friends.js
@@ -0,0 +1,37 @@
+var friends = [
+    {
+        name: "Arya Stark",
+        photo: "assets/images/aryaStark",
+        scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
+    },
+    {
+        name: "Jon Snow",
+        photo: "assets/images/jonSnow",
+        scores: [4, 2, 5, 1, 3, 2, 2, 1, 3, 2]
+    },
+    {
+        name: "Tyrion Lannister",
+        photo: "assets/images/tyrionLannister",
+        scores: [5, 2, 2, 2, 4, 1, 3, 2, 5, 5]
+    },
+    {
+        name: "Daenerys Targaryen",
+        photo: "assets/images/daenerysTargaryen",
+        scores: [3, 3, 4, 2, 2, 1, 3, 2, 2, 3]
+    },
+    {
+        name: "Sansa Stark",
+        photo: "assets/images/sansaStark",
+        scores: [4, 3, 4, 1, 5, 2, 5, 3, 1, 4]
+    },
+    {
+        name: "Cersei Lannister",
+        photo: "assets/images/cerseiLannister",
+        scores: [4, 3, 4, 2, 4, 3, 5, 3, 5, 4]
+    },
+    {
+        name: "Ice King",
+        photo: "assets/images/iceKing",
+        scores: [4, 4, 2, 3, 2, 2, 3, 2, 4, 5]
+    }
+];
